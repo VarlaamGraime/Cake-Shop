d3542db07003b21fbffdc0808ef8167df65f8dbd
VarlaamGraime <108095273+VarlaamGraime@users.noreply.github.com>
2023-03-21 21:46:56 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import AppRoute from "./components/AppRoute";
 import NavBar from './components/NavBar';
 
