ec16ba9e46dcdca52941089633cd68850fd12fc3
VarlaamGraime <108095273+VarlaamGraime@users.noreply.github.com>
2023-03-21 22:22:50 +0300

    fix

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,7 +1,7 @@
 export const ADMIN_ROUTE = '/admin';
 export const LOGIN_ROUTE = '/login';
 export const REGISTRATION_ROUTE = '/registration';
-export const SHOP_ROUTE = '/';
+export const SHOP_ROUTE = 'https://varlaamgraime.github.io/Cake-Shop/';
 export const BASKET_ROUTE = '/basket';
 export const CAKE_ROUTE = '/cake';
 export const CONSTR = '/constructor'
