fbf17b94eed233920dc155f9c02f9030f54d04af
VarlaamGraime <108095273+VarlaamGraime@users.noreply.github.com>
2023-03-21 21:34:46 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter >
       <NavBar />
       <AppRoute />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
