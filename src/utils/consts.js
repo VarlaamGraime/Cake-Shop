e3ede97bb2eea9a292bea77964ca07b10feb9727
VarlaamGraime <108095273+VarlaamGraime@users.noreply.github.com>
2023-03-21 22:14:04 +0300

    Updates

--- src/utils/consts.js
+++
@@ -1,8 +0,0 @@
-export const ADMIN_ROUTE = '/admin';
-export const LOGIN_ROUTE = '/login';
-export const REGISTRATION_ROUTE = '/registration';
-export const SHOP_ROUTE = '/';
-export const BASKET_ROUTE = '/basket';
-export const CAKE_ROUTE = '/cake';
-export const CONSTR = '/constructor'
-
